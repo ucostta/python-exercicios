408eb882790adb5f7f607a161d0386ada50bf330
ucostta <gustavofixo04@gmail.com>
2024-05-07 10:16:42 -0300

    exercicio 1

---
+++ entrada_saida/exercicio_1.py
@@ -0,0 +1,15 @@
+# Ler o primeiro número inteiro do usuário
+numero1 = int(input("Digite o primeiro número inteiro: "))
+
+# Ler o segundo número inteiro do usuário
+numero2 = int(input("Digite o segundo número inteiro: "))
+
+# Calcular a soma dos números
+soma = numero1 + numero2
+
+# Calcular o produto dos números
+produto = numero1 * numero2
+
+# Imprimir os resultados
+print("A soma dos dois números é:", soma)
+print("O produto dos dois números é:", produto)
