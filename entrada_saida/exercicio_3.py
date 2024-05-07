6c08eb9b231863c5658b123d1e5fb43201eacd24
ucostta <gustavofixo04@gmail.com>
2024-05-07 10:39:46 -0300

    exercicio 3

---
+++ entrada_saida/exercicio_3.py
@@ -0,0 +1,9 @@
+# Ler a base e a altura do triângulo do usuário
+base = float(input("Digite o valor da base do triângulo: "))
+altura = float(input("Digite o valor da altura do triângulo: "))
+
+# Calcular a área do triângulo
+area = (base * altura) / 2
+
+# Imprimir o resultado
+print("A área do triângulo é:", area)
