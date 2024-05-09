8737025634a3e1c6081925b937c0fb2adf926b3e
ucostta <gustavofixo04@gmail.com>
2024-05-09 15:56:45 -0300

    atualização exercicio1.py

--- condicionais/exercicio_1.py
+++ condicionais/exercicio_1.py
@@ -14,7 +14,28 @@ try:
     # Ler o conteudo da resposta
     data = response.read().decode('utf-8')
 
-    # Teste...
-    print(data)
+    # Converter JSON em dicionario Python
+    endereço = json.loads(data)
+
+    #Verificar se a consulta foi bem sucedida
+    if endereço.get('erro'):
+        print("CEP não encontrado")
+    else:
+        # Armazenar informações em variáveis
+        logradouro = endereço['logradouro']
+        complemento = endereço['complemento']
+        bairro = endereço['bairro']
+        cidade = endereço['localidade']
+        estado = endereço['uf']
+
+        # Exibir informações do endereço na tela
+        print(f"Logradouro: {logradouro}")
+        print(f"Complemento: {complemento}")
+        print(f"Bairro: {bairro}")
+        print(f"Cidade: {cidade}")
+        print(f"Estado: {estado}")
+
+    # Fechar a conexão
+    response.close()
 except Exception as e:
     print(f"Erro: {e}")
